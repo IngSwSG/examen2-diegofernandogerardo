a953992e856454c9f1cc7a97ce9f335bbe6634b5
Gerardo0899 <GERARDO.JIMENEZVILLALOBOS@UCR.AC.CR>
2024-07-03 14:40:06 -0600

    creacion rama

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Models;
-
+//cambio prueba
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
